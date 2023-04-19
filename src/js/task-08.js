4d133841620353020e92cea4768e268d6154bfbd
AnnaBurkova <anna.burkova@ukr.net>
2023-04-19 22:43:43 +0300

    Додає вирішення задач №7,8,9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
 
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const formObject = Object.fromEntries(formData.entries());
+
+  if (!formObject.email || !formObject.password) {
+    alert('All fields must be filled!');
+    return;
+  }
+
+  console.log(formObject);
+  loginForm.reset();
+});
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+changeColorButton.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
