003e414447216b6d74aad7f6773078922f3bb57a
AnnaBurkova <anna.burkova@ukr.net>
2023-04-29 21:59:26 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
 const inputRange = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
+text.style.fontSize = `${inputRange.value}px`;
+
 
 inputRange.addEventListener('input', onRangeChange);
 function onRangeChange(event) {
